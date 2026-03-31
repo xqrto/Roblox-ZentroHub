a67caf8916ec4ffaa3f6e569e489f6935aeb7114
xqrto <janfelixlindner@gmail.com>
2026-03-31 13:06:26 +0200

    Emotes

--- Zentro.lua
+++ Zentro.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xqrto/Roblox-ZentroHub/main/ZentroHub.lua?nocache="..os.time()))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xqrto/Roblox-ZentroHub/main/ZentroEmotes.lua?nocache="..os.time()))()
